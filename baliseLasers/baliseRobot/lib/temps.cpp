27c8699a212c5a103419403932006f70a83398a2
Guillaume DEMAISON <guillaume.demaison@minet.net>
2010-11-21 17:14:14 +0100

    Premier commit du code de la balise laser. Ajout du code de commande de la tourelle du robot.

---
+++ baliseLasers/baliseRobot/lib/temps.cpp
@@ -0,0 +1,21 @@
+#include "temps.h"
+
+volatile uint32_t cnt_ovf_timer1=0;//compteur d'interruptions timer1
+
+void temps_init() {
+	cbi(TCCR1B,CS12);//|choix d'une clock 
+	cbi(TCCR1B,CS11);//|sans prescaler pour 
+	sbi(TCCR1B,CS10);//|plus de précision
+	sbi(TIMSK1,TOIE1);//pour activer les interruptions sur overflow
+}
+
+uint32_t micros() {
+	uint32_t microseconds = 0;
+	microseconds = (cnt_ovf_timer1*65536+TCNT1H*256+TCNT1L)/(F_CPU/1000000);
+	return microseconds;
+}
+
+ISR(TIMER1_OVF_vect) {
+	cnt_ovf_timer1++;
+}
+
---
+++ baliseLasers/baliseRobot/main.cpp
@@ -0,0 +1,151 @@
+#include "main.h"
+
+/*
+ * définissons quelques variables globales ou define (selon les 
+ * besoins) qu'on va utiliser pour le moteur.
+ */
+//pour la consigne en vitesse de rotation du moteur : 
+#define freqD 10 //fréquence demandée en tr/s
+static uint32_t consigne;//période pour la consigne de commande
+//pour la mesure des periodes de rotation du moteur
+uint32_t temps[2]={65535,65535};//contient les périodes de rotation mesurées
+uint32_t tempsI=65535;//période instantannée de rotation mesurée
+uint8_t indT=0;//code l'élément de temps[] à remplir
+uint32_t milAP=0;//micros après top
+uint32_t milAV=0;//micros avant top
+//pour l'asservissement en vitesse du moteur
+//static uint16_t timerAsserv=10000;//période d'asservissement
+//uint32_t asservAv = 0;//compteur de temps pour l'asservissement
+//static int16_t kP = 1000;//coefficient de conversion entre la période de consigne et la période de commande (dépend du nombre de pas/tr du moteur et du nombre de commandes par pas).
+//pour la commande du moteur
+uint16_t periode = 40000;//periode de commande du moteur, en microsecondes
+uint16_t incPeriode = 5000; //periode d'incrémentation de periode pour le démarrage, en microseconde
+uint32_t incTemps = 0;//stoque le temps écoulé depuis le début pour le démarrage
+uint32_t micTemps = 0;//stoque le temps écoulé depuis le début pour les commutions du moteur
+uint8_t ind = 0;//code la position dans le chronogramme de commande du moteur
+
+/**
+ * et on y va pour la fonction main
+ */
+int main() {
+	int temp=0;
+	//on initialise la notion temporelle sur l'AVR
+	temps_init();
+	//on initialise la transmission série (pour le debug)
+	uart_init();
+	//on initialise les ports de commande du moteur
+	sbi(DDRB,pinMot1);
+	sbi(DDRB,pinMot2);
+	sbi(DDRB,pinMot3);
+	//on initialise les interruptions du top tour sur front descendant
+	sbi(EICRA,ISC01);
+	cbi(EICRA,ISC00);
+	sbi(EIMSK,INT0);//pour activer les interruptions sur INT0
+	//on initialise la pin du sélecteur de sens en input avec pull-up
+	cbi(DDRC,pinSens);//input
+	sbi(portSens,pinSens);//pull-up
+
+	//on calcule la valeur de la consigne en microsecondes en fonction de la fréquence de rotation demandée
+	consigne=1000000/freqD;
+
+	//et on y va ;)
+	printlnString("Go");
+
+	//démarrage : 
+	printlnString("demarrage");
+	while (periode > 5556) {
+		//calcul de la valeur de periode à utiliser ici : 
+		if (micros() > incTemps + incPeriode) {
+			incTemps = micros();
+			if (periode > 10000) {
+				periode--;
+			}
+			else if (periode > 2778) {
+				periode--;
+			}
+		}
+		//commutation des bobines du moteur : 
+		if (micros() > micTemps + periode) {
+			temp=PINC;
+			printUInt(temp);
+			printString("|");
+			printUInt((temp&(1<<PINC1))>>PINC1);
+			printString("|");
+			printUShort(PINC0);
+			printUShort(PINC1);
+			printUShort(PINC2);
+			printUShort(PINC3);
+			printUShort(PINC4);
+			printUShort(PINC5);
+			printUShort(PINC6);
+			printString("\t");
+			printUInt(periode);
+			printString("d");
+			printULong(temps[1]);
+			printString("\t");
+			printlnInt(1000000/temps[1]);
+			micTemps = micros();
+			if (ind==0)		cbi(portMot3,pinMot3);
+			else if (ind==1)	sbi(portMot2,pinMot2);
+			else if (ind==2)	cbi(portMot1,pinMot1);
+			else if (ind==3)	sbi(portMot3,pinMot3);
+			else if (ind==4)	cbi(portMot2,pinMot2);
+			else			sbi(portMot1,pinMot1);
+			if (ind<5) 		ind++;
+			else 			ind = 0;
+		}
+	}
+	periode=5556;
+	while (1) {
+		//commutation des bobines du moteur : 
+		if (micros() > micTemps + periode) {
+			temp=PINC;
+			printUInt(temp);
+			printString("|");
+			printUInt((temp&(1<<PINC1))>>PINC1);
+			printString("|");
+			printUShort(PINC0);
+			printUShort(PINC1);
+			printUShort(PINC2);
+			printUShort(PINC3);
+			printUShort(PINC4);
+			printUShort(PINC5);
+			printUShort(PINC6);
+			printString("\t");
+			printUInt(periode);
+			printString("d");
+			printULong(temps[1]);
+			printString("\t");
+			printlnInt(1000000/temps[1]);
+			micTemps = micros();
+			if (ind==0)		cbi(portMot3,pinMot3);
+			else if (ind==1)	sbi(portMot2,pinMot2);
+			else if (ind==2)	cbi(portMot1,pinMot1);
+			else if (ind==3)	sbi(portMot3,pinMot3);
+			else if (ind==4)	cbi(portMot2,pinMot2);
+			else			sbi(portMot1,pinMot1);
+			if (ind<5) 		ind++;
+			else 			ind = 0;
+		}
+	}
+	return 0;
+}
+
+/**
+ * fonction du top-tour
+ */
+ISR(INT0_vect) {
+	cli();
+	milAP=micros();
+	tempsI=(milAP-milAV);
+	milAV=milAP;
+	if (indT<1) {
+		indT++;
+	}
+	else {
+		indT=0;
+	}
+	temps[0]=temps[1];
+	temps[1]=tempsI;
+	sei();
+}
