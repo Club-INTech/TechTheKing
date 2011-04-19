4f6885803e34b029950df38fbaac6388e0401900
Vincent Jacquot <vincent.jacquot@telecom-sudparis.eu>
2011-04-19 16:34:44 +0200

    Code pour les ultrasons

--- baseRoulante/AVR/capteurs/ultrasons.c
+++ baseRoulante/AVR/capteurs/ultrasons.c
@@ -1,1 +1,36 @@
- 
+#include "ultrasons.h"
+
+uint16_t ping(uint8_t pin)
+{
+    // Envoi d'une impulsion dans le capteur
+    DDRB |= pin;
+    PORTB &= ~pin;
+    _delay_us(2);
+    PORTB |= pin;
+    _delay_us(5);
+    PORTB &= ~pin;
+
+    // Reception de la duree
+    DDRB &= ~pin;
+
+    uint8_t masque = pin;
+
+    uint16_t duree = 0;
+    uint16_t duree_max = TIMEOUT;
+
+    // Attente de la fin de l'impulsion precedente
+    while ((PINB & pin) == masque)
+        if (duree++ == duree_max)
+            return 0;
+
+    // Attente du demarrage de l'impulsion
+    while ((PINB & pin) != masque)
+        if (duree++ == duree_max)
+            return 0;
+
+    // Attente de la fin de l'impulsion
+    while ((PINB & pin) == masque)
+            duree++;
+
+    return duree;
+} 
--- baseRoulante/AVR/capteurs/ultrasons.h
+++ baseRoulante/AVR/capteurs/ultrasons.h
@@ -1,1 +1,22 @@
- 
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+/*
+ *  Pins des ultrasons relies a la carte
+ *  TODO : Mettre les vraies valeurs
+ */
+#define PIN_ULTRASON_1  (1 << PORTB5)
+#define PIN_ULTRASON_2  (1 << PORTB4)
+
+/*
+ *  Temps maximal d'attente de la fin d'une impulsion
+ *    S'exprimme en nombre de passage dans une boucle, donc depend de cette boucle
+ */
+#define TIMEOUT 750
+
+/*
+ *  Fonction pour obtenir la distance a partir d'un capteur a ultrasons
+ *    Prend la pin du capteur en argument
+ */
+uint16_t ping(uint8_t pin);
