153edab915fcf6e2685ea21754f9624c637ed93b
intech <intech@intech-701.(none)>
2011-05-10 18:04:57 +0200

    before merge

--- baseRoulante/AVR/asservissement_2/lib/Manager.cpp
+++ baseRoulante/AVR/asservissement_2/lib/Manager.cpp
@@ -1,7 +1,7 @@
 #include "Manager.h"
 
-#define CONVERSION_TIC_ANGLE 48.444097 //(2 * Pi * 65 536) / 8500
-#define CONVERSION_TIC_DISTANCE 0.0000158239294 // 3375/(3500 * 65536)
+#define CONVERSION_TIC_ANGLE 48.330379 //(2 * Pi * 65 536) / 8500
+#define CONVERSION_TIC_DISTANCE 0.00000158239294 // 3375/(3500 * 65536)
 #define ABS(x) (x > 0 ? x : -x)
 
 volatile long x;
@@ -172,14 +172,14 @@ void Manager::init()
 	indiceConsigneActuelle=1;
 
 	// initialisation des constantes
-	assRotation.changeKp(10);
+	assRotation.changeKp(8);
 	assRotation.changePWM(PWM_MAX);
 	assRotation.changeKd(300);
 	assRotation.changeKi(0);
 	assRotation.changeVmax(0);
 	assRotation.changeKpVitesse(0);
 
-	assTranslation.changeKp(10);
+	assTranslation.changeKp(8);
 	assTranslation.changePWM(PWM_MAX);
 	assTranslation.changeKd(300);
 	assTranslation.changeKi(0);
