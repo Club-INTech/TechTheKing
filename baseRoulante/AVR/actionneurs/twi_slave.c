c7c8ace074560f2763591e107c9cedc00b8ae5e6
Vincent Jacquot <vincent.jacquot@gmail.com>
2011-05-28 18:56:23 +0200

    APO! PANTOZ! KAKO DEMONOZ

--- baseRoulante/AVR/actionneurs/twi_slave.c
+++ baseRoulante/AVR/actionneurs/twi_slave.c
@@ -62,7 +62,7 @@ void TWI_Loop( void )
                 cons = AX_ANGLE_EXT1;
             
             // Envoi de l'ordre au servo
-            AX12GoTo (AX_ID1, cons);
+            AX12GoTo (0XFE, cons);
         }
 
         // Ordre pour l'AX12 2
@@ -80,7 +80,7 @@ void TWI_Loop( void )
                 cons = AX_ANGLE_EXT1;
             
             // Envoi de l'ordre au servo
-            AX12GoTo (AX_ID2, cons);
+            AX12GoTo (0XFE, cons);
         }
 
         else if ( messageBuf[0] == MASTER_CMD_SERVO1_UP ) {
