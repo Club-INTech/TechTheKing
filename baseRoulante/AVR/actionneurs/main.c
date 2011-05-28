611d7e3d4364064aa261667c6d04e77df10b56ef
Jérémy Cheynet <jeremy@minet.net>
2011-05-29 01:22:39 +0200

    Merge

--- baseRoulante/AVR/actionneurs/main.c
+++ baseRoulante/AVR/actionneurs/main.c
@@ -30,6 +30,7 @@ int main()
 
 	SERVO_CONS1 = SERVO_PWM_UP1;
 	SERVO_CONS2 = SERVO_PWM_UP2;
+	consigne1 = 5000;
 
 	while(1)
 	{
@@ -42,7 +43,7 @@ int main()
 
 		/********************* I2C ********************/
 		// Je l'ai dans l'interruption TWI
-		// TWI_Loop();
+		TWI_Loop();
 		/**********************************************/
 
 	//         /*** Pour les tests ***/
@@ -62,55 +63,4 @@ int main()
 	}
 
 	return 0;
-=======
-    // Pour le debug serie
-    //uart_init();
-    
-    // Initialisations diverses (et variees)
-    actio_init();
-    
-    // Initialisation I2C
-    TWI_Init();
-    
-    // Initialisation pour l'AX12 gauche
-    AX12Init (AX_ID1, AX_ANGLE_EXT1, AX_ANGLE_EXT2, AX_SPEED);
-    // Initialisation pour l'AX12 droite
-    AX12Init (AX_ID2, AX_ANGLE_EXT1, AX_ANGLE_EXT2, AX_SPEED);
-    
-    SERVO_CONS1 = SERVO_PWM_UP1;
-    SERVO_CONS2 = SERVO_PWM_UP2;
-    
-	consigne1 = 5000;
-
-    while(1)
-    {   
-        /*************** Asservissement ***************/
-        if (etat_asservissement == ASC_ASSERV_SYNCHRO)
-           asservissement_synchro();
-        else if (etat_asservissement == ASC_ASSERV_INDEP)
-           asservissement();
-        /**********************************************/
-        
-        /********************* I2C ********************/
-        TWI_Loop();
-        /**********************************************/
-        
-//         /*** Pour les tests ***/
-//         _delay_ms(2000);
-//         //SERVO2 = PWM_UP;
-//         AX12GoTo(0XFE, 511);
-//         _delay_ms(2000);
-//         //SERVO2 = PWM_DOWN;
-//         AX12GoTo(0XFE, 200);
-//         _delay_ms(2000);
-//         AX12GoTo(0XFE,800);
-
-//         _delay_ms(250);
-//         printLong(ascenseur1);
-//         printString(" ; ");
-//         printlnLong(ascenseur2);
-    }
-    
-    return 0;
->>>>>>> 98c14071f3c7daef071740fd44cd979deb86c13e
 }
