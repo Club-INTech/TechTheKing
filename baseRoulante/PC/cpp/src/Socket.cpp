6f97df3872d31dd87f60d225b1f78f678e4140bb
Philippe Tillet <Phil.Tillet@gmail.com>
2011-05-29 22:17:23 +0200

    Je m'présente, je m'appelle Henri.

--- baseRoulante/PC/cpp/src/Socket.cpp
+++ baseRoulante/PC/cpp/src/Socket.cpp
@@ -4,6 +4,9 @@
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 #include <Magick++.h>
+
+#define TOLERANCE_DISTANCE 10
+
 Socket* Socket::m_instance=NULL;
 
 Socket::Socket(int port){
@@ -26,12 +29,16 @@ Socket* Socket::Instance(int port){
 
 void Socket::getPions(){
     boost::thread_group group;
-    //group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.3"));
-    group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.2"));
-    //group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.4"));
+    std::vector<Obstacle*> listeObstacles1;
+    std::vector<Obstacle*> listeObstacles2;
+    std::vector<Obstacle*> listeObstacles3;
+    group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.2",listeObstacles1));
+    group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.3",listeObstacles2));
+    //group.create_thread(boost::bind(&Socket::getPions,this,"192.168.6.4",listeObstacles3));
     group.join_all();
+    
 }
-void Socket::getPions(const char* address){
+void Socket::getPions(const char* address,std::vector<Obstacle*>& Obstacles){
     int port = 42000;
     int sockfd = getFd(address);
     struct hostent *server;
@@ -52,8 +59,9 @@ void Socket::getPions(const char* address){
         std::cerr << "Could not connect\n" << std::endl;
     }
     onWrite(sockfd,"pions");
-    std::vector<Obstacle*> ranranru = trouverObstacles(onRead(sockfd));
-    
+    trouverObstacles(onRead(sockfd),Obstacles);
+
+	/*
     #ifdef DEBUG_GRAPHIQUE
     Magick::Image image( "img/table.png" );
     for(std::vector<Obstacle *>::iterator it=ranranru.begin();it!=ranranru.end();it++){
@@ -61,6 +69,7 @@ void Socket::getPions(const char* address){
     }
     image.display();
     #endif
+    */
     
     close(sockfd);
 }
@@ -112,11 +121,10 @@ int Socket::getFd(const char* address){
 void Socket::onClose(){
 }
 
-std::vector<Obstacle*> Socket::trouverObstacles(std::string trame){
-    std::vector<Obstacle*> result;
+void Socket::trouverObstacles(std::string trame, std::vector<Obstacle*>& Obstacles){
     std::string x,y;
     if(trame[0]!='d'){
-        return result;
+        return ;
     }
     else{
         int i=1;
@@ -127,7 +135,7 @@ std::vector<Obstacle*> Socket::trouverObstacles(std::string trame){
                 case 'x':
                     if(currentCoord=='y'){
                         std::cout << "x = " << x << " y = " << y << std::endl;
-                        result.push_back(new CercleObstacle(atoi(x.c_str()),atoi(y.c_str())));
+                        Obstacles.push_back(new CercleObstacle(atoi(x.c_str()),atoi(y.c_str())));
                     }
                     x="";
                     y="";
@@ -147,6 +155,82 @@ std::vector<Obstacle*> Socket::trouverObstacles(std::string trame){
                     }
             }
         }
-        return result;
     }
 }
+
+// Intersection des résultats (au moins 2 téléphones nécessaires)
+std::vector< std::pair<Obstacle*,int> > Socket::fusionResultats(std::vector<Obstacle*> t1, std::vector<Obstacle*> t2, std::vector<Obstacle*> t3, int niveau)
+{
+	std::vector<Obstacle*>::iterator it;
+	std::vector< std::pair<Obstacle*,int> > resultatFusion;
+	
+	// Une double boucle pour 3 instructions c'est mal
+	for(it=t1.begin();it!=t1.end();it++)
+	{
+		ajouterPion(resultatFusion,it);
+	}
+	
+	for(it=t2.begin();it!=t2.end();it++)
+	{
+		ajouterPion(resultatFusion,it);
+	}
+	
+	for(it=t3.begin();it!=t3.end();it++)
+	{
+		ajouterPion(resultatFusion,it);
+	}
+	
+	// Tri des résultats en fonction du niveau
+	for(std::vector< std::pair<Obstacle*,int> >::iterator it2=resultatFusion.begin();it2!=resultatFusion.end();it2++)
+	{
+		if (it2->second < niveau) resultatFusion.erase(it2);
+	}
+	
+	return resultatFusion;
+}
+
+// True si les 2 pions sont les mêmes, false sinon 
+bool Socket::comparerPions(std::vector< std::pair<Obstacle*,int> >::iterator a, std::vector<Obstacle*>::iterator b)
+{
+	double d = (a->first)->rayon(**b);
+	return (d < TOLERANCE_DISTANCE);
+}
+
+// Vérifie si un pion est déjà présent, l'ajoute sinon
+void Socket::ajouterPion(std::vector< std::pair<Obstacle*,int> > &v, std::vector<Obstacle*>::iterator p)
+{
+	std::vector< std::pair<Obstacle*,int> >::iterator it;
+	bool present = false;
+	
+	for(it=v.begin();it!=v.end();it++)
+	{
+		// Si pion déjà présent, on augmente le poids
+		if (comparerPions(it,p))
+		{
+			present = true;
+			
+			// Calcul du barycentre
+			((it->first))->setX( ( it->second * ((it->first))->getX() + (*p)->getX() ) / (it->second + 1) );
+			((it->first))->setY( ( it->second * ((it->first))->getY() + (*p)->getY() ) / (it->second + 1) );
+			
+			// Incrémentation du poids
+			it->second += 1;
+		}
+	}
+	
+	// Ajout du pion s'il n'est pas déjà présent
+	if (!present)
+	{
+		// new CercleObstacle((*p)->getX(),(*p)->getY()) ?
+		v.push_back(std::make_pair<Obstacle*,int>(new CercleObstacle((*p)->getX(),(*p)->getY()),1));
+	}
+}
+
+void Socket::printVector(std::vector<Obstacle*> v)
+{	
+	for(std::vector<Obstacle*>::iterator it=v.begin();it!=v.end();it++)
+	{
+		std::cout << (*it)->getX() << " " << (*it)->getY() << std::endl;
+	}
+}
+
