72fdb1877e6dde60c33d8e5458ab57787c542a29
Vincent Jacquot <vincent.jacquot@gmail.com>
2011-05-31 18:13:41 +0200

    Ultrasons

--- baseRoulante/AVR/capteur/main.c
+++ baseRoulante/AVR/capteur/main.c
@@ -13,19 +13,19 @@ int main()
     uart_init();
     
     while(1) {
-        _delay_ms(10);
+        _delay_ms(25);
         
         uint32_t temp1 = 0;
         uint32_t temp2 = 0;
         
         uint8_t i;
-        for (i = 0; i < 4 ; i++) {
+        for (i = 0; i < 2 ; i++) {
             // Polling ultrason
             temp1 += ping(PIN_ULTRASON1);
             temp2 += ping(PIN_ULTRASON2);
         }
         
-        ultrason = (temp1 + temp2) >> 3;
+        ultrason = (temp1 + temp2) >> 2;
     }
 
     return 0;
