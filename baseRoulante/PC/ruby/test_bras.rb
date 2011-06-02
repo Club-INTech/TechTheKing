a9aa279c4a7b9c63557e8b25ee3e4142bbb00c04
Vincent Jacquot <vincent.jacquot@gmail.com>
2011-06-02 14:44:15 +0200

    meilleur test bras

--- baseRoulante/PC/ruby/test_bras.rb
+++ baseRoulante/PC/ruby/test_bras.rb
@@ -6,46 +6,46 @@ InterfaceActionneurs = LibChessUp::InterfaceActionneurs.new;
 
 sleep(1);
 
-#InterfaceActionneurs.positionAimantGauche(LibChessUp::BAS);
+InterfaceActionneurs.positionAimantGauche(LibChessUp::BAS);
 InterfaceActionneurs.positionAimantDroit(LibChessUp::BAS);
 
 sleep(1);
 
-#InterfaceActionneurs.hauteurBrasDroit(LibChessUp::SOCLE);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
-
-sleep(1);
-
-#InterfaceActionneurs.hauteurBrasDroit(LibChessUp::CAPTURE);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::CAPTURE);
-
-sleep(1);
-
-#InterfaceActionneurs.hauteurBrasDroit(LibChessUp::TOUR);
+InterfaceActionneurs.hauteurBrasDroit(LibChessUp::CAPTURE);
 InterfaceActionneurs.hauteurBrasGauche(LibChessUp::TOUR);
 
 sleep(1);
 
-#InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
+InterfaceActionneurs.hauteurBrasDroit(LibChessUp::TOUR);
+InterfaceActionneurs.hauteurBrasGauche(LibChessUp::CAPTURE);
+
+sleep(1);
+
+InterfaceActionneurs.angleBrasDroit(LibChessUp::REPLIE);
+InterfaceActionneurs.angleBrasGauche(LibChessUp::REPLIE); 
+
+sleep(1);
+
+InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
 InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT); 
 
 sleep(1);
 
-#InterfaceActionneurs.angleBrasDroit(LibChessUp::BALAYAGE);
+InterfaceActionneurs.angleBrasDroit(LibChessUp::BALAYAGE);
 InterfaceActionneurs.angleBrasGauche(LibChessUp::BALAYAGE); 
 
 sleep(1);
 
-#InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
+InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
 InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT);
 
 sleep(1);
 
-#InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
+InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
 InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
 
 sleep(1);
 
 
-#InterfaceActionneurs.positionAimantGauche(LibChessUp::HAUT);
+InterfaceActionneurs.positionAimantGauche(LibChessUp::HAUT);
 InterfaceActionneurs.positionAimantDroit(LibChessUp::HAUT);
