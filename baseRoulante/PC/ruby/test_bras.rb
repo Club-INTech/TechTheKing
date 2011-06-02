3fbc7abe0c3546b34a89d97818a6fbf1f6a23e6d
Vincent Jacquot <vincent.jacquot@gmail.com>
2011-06-02 11:25:38 +0200

    dfsdf

--- baseRoulante/PC/ruby/test_bras.rb
+++ baseRoulante/PC/ruby/test_bras.rb
@@ -7,45 +7,45 @@ InterfaceActionneurs = LibChessUp::InterfaceActionneurs.new;
 sleep(1);
 
 InterfaceActionneurs.positionAimantGauche(LibChessUp::BAS);
-InterfaceActionneurs.positionAimantDroit(LibChessUp::BAS);
+#InterfaceActionneurs.positionAimantDroit(LibChessUp::BAS);
 
 sleep(1);
 
 InterfaceActionneurs.hauteurBrasDroit(LibChessUp::SOCLE);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
+#InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
 
 sleep(1);
 
 InterfaceActionneurs.hauteurBrasDroit(LibChessUp::CAPTURE);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::CAPTURE);
+#InterfaceActionneurs.hauteurBrasGauche(LibChessUp::CAPTURE);
 
 sleep(1);
 
 InterfaceActionneurs.hauteurBrasDroit(LibChessUp::TOUR);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::TOUR);
+#InterfaceActionneurs.hauteurBrasGauche(LibChessUp::TOUR);
 
 sleep(1);
 
 InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
-InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT); 
+#InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT); 
 
 sleep(1);
 
 InterfaceActionneurs.angleBrasDroit(LibChessUp::BALAYAGE);
-InterfaceActionneurs.angleBrasGauche(LibChessUp::BALAYAGE); 
+#InterfaceActionneurs.angleBrasGauche(LibChessUp::BALAYAGE); 
 
 sleep(1);
 
 InterfaceActionneurs.angleBrasDroit(LibChessUp::DROIT);
-InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT);
+#InterfaceActionneurs.angleBrasGauche(LibChessUp::DROIT);
 
 sleep(1);
 
 InterfaceActionneurs.hauteurBrasDroit(LibChessUp::SOCLE);
-InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
+#InterfaceActionneurs.hauteurBrasGauche(LibChessUp::SOCLE);
 
 sleep(1);
 
 
 InterfaceActionneurs.positionAimantGauche(LibChessUp::HAUT);
-InterfaceActionneurs.positionAimantDroit(LibChessUp::HAUT);
+#InterfaceActionneurs.positionAimantDroit(LibChessUp::HAUT);
