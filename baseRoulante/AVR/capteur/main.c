890a3b39b1270a31236eb4bbfd1b58802721066d
Vincent Jacquot <vincent.jacquot@gmail.com>
2011-06-02 06:03:13 +0200

    Activation double ultrasons

--- baseRoulante/AVR/capteur/main.c
+++ baseRoulante/AVR/capteur/main.c
@@ -12,16 +12,16 @@ int main()
     // Initialisation serie
     uart_init();
     
-//     volatile uint16_t temp1;
-//     volatile uint16_t temp2;
+    volatile uint16_t temp1;
+    volatile uint16_t temp2;
     
     while(1) {
         _delay_ms(50);
         ultrason = ping(PIN_ULTRASON1);
-//         ultrason = temp1?(temp1<temp2):temp2;
-//         _delay_ms(50);
-//         temp2 = ping(PIN_ULTRASON2);
-//         ultrason = temp2?(temp2<temp1):temp1;
+        ultrason = temp1?(temp1<temp2):temp2;
+        _delay_ms(50);
+        temp2 = ping(PIN_ULTRASON2);
+        ultrason = temp2?(temp2<temp1):temp1;
     }
     
     return 0;
